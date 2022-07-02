011678d9e62d13a9f62e44b16b37251342260d35
jpal91 <pallanschja@gmail.com>
2022-07-02 12:22:13 -0400

    first commit

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,20 +1,74 @@
 import React, { useEffect, useState } from "react";
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
 import ReactHtmlParser from "react-html-parser";
+import hljs from "highlight.js";
 
 import { searchText } from "../helpers/search-text";
 
+const transform = (node, index) => {
+    // if (node.parent && node.parent.name === 'code' && node.type === 'tag') {
+    //     if (node.name === 'br') {
+    //         return `\n`
+    //     }
+        
+    //     let textMap = node.children.map((d) => {
+    //         return `${d.data}`
+    //     }).join('')
+    //     return (
+    //         `<${node.name}>${textMap}</${node.name}>`
+    //     )
+    // }
+    // if (!node.parent && node.type === 'tag' && !node.attribs.id) { node.attribs.id = index }
+    // if (!node.parent && node.type === 'text') {
+        
+    //     return (
+    //         <span id={index}>{node.data}</span>
+    //     )
+    // }
+}
+
 const Data = () => {
   const [data, setData] = useState();
-  const handleChange = (event) => setData(event.target.value);
+  const handleChange = (e) => {
+    // e.preventDefault()
+    // setData(e.currentTarget.textContent)
+    setData(e.target.value);
+}
 
   useEffect(() => {
-    const filePath = path.join(process.cwd(), "README.md");
-    fs.readFile(filePath, "utf8", (err, data) => {
-      setData(data);
-    });
+    // const filePath = path.join(process.cwd(), "README.md");
+    // fs.readFile(filePath, "utf8", (err, data) => {
+    //   setData(data);
+    // });
+    const gettingData = async () => {
+        await fetch('./test.md')
+        .then(res => res.text())
+        .then(res => setData(res))
+        //.then(res => console.log(res))
+    }
+    gettingData()
+
+    const textId = document.querySelector('textarea')
+
+    textId.addEventListener('keydown', (e) => {
+        console.log(e)
+        if (e.keyCode === 9) {
+            e.preventDefault()
+            textId.setRangeText(
+                ' ',
+                textId.selectionStart,
+                textId.selectionStart,
+                'end'
+            )
+        }
+    })
+
   }, []);
+
+  useEffect(() => {
+
+  }, [data])
 
   //const returnedData = data && data.split(/[\n]+/).map((d) => searchText(d))
   const returnedData = data && searchText(data);
@@ -25,9 +79,13 @@ const Data = () => {
         rows="20"
         cols="100"
         value={data}
+        tabIndex='0'
         onChange={(event) => handleChange(event)}
+        
+        id='my-textbox'
       />
-      <div>{ReactHtmlParser(returnedData)}</div>
+      {/* <pre contentEditable='true' id="my-textbox" width="1000" wrap="true" onInput={(e) => {handleChange(e)}}>{data}</pre> */}
+      <div id="daddy">{ ReactHtmlParser(returnedData, { transform: transform } ) }</div>
     </React.Fragment>
   );
 };
--- src/helpers/search-text.js
+++ src/helpers/search-text.js
@@ -1,17 +1,217 @@
+import hljs from "highlight.js";
 
 
 export const searchText = (str) => {
-  let regex = /(?<=#\s)[A-Z a-z]*/;
-  let regex2 = /(```js)[\n\s]+[A-Z a-z]*[\n\s]+(```)/gm;
-  // if (!regex.test(str)) {
-  //     return <p>{str}</p>
-  // }
+    // const bracketRegex = /(`"?<.*>"?`|<.*>|<.*|<)(?!<\/code>)/g
+    const openBracketRegex = /.<.*/g
+    const bracketRegex = /`<.+>.*<\/.+>`/g
+    const headingsRegex = /#+\s.+\n/g;
+    const linksRegex = /\[.+\]\(.+\)/g
+    const boldRegex = /(?<!(\*|\S))\*\*[^*\n]+\*\*(?!\*)/g
+    const italicRegex = /(?<!(\*|\S))\*[^*\n]+\*(?!\*)/g
+    const codeRegex = /`[^<>\n`]+`/g
+    const imageRegex = /!\[.*\]\(.+\)/g
+    const blockCodeRegex =  /```js\n(?:(?!```)[\s\S])+\n```/g///^```js\n(.*\n)+```\n$/gm
 
-  let newString = str.match(regex);
+    const hasOpenBracket = str.match(openBracketRegex)
+    hasOpenBracket ? str = openBrackets(hasOpenBracket, str) : str = str
 
-  let temp = str.replace(newString[0], `<h3>${newString[0]}</h3>`);
-  temp = temp.replace(/[\n]+/g, "</br>");
-  //console.log(temp.split(/[\n]+/))
+    const hasBlockCode = str.match(blockCodeRegex)
+    hasBlockCode ? str = blockCode(hasBlockCode, str) : str = str
+    // console.log(str)
+    const hasBracket = str.match(bracketRegex)
+    hasBracket ? str = bracket(hasBracket, str) : str = str
+    //console.log(str)
 
-  return temp;
+
+
+    // openBracketRegex.test(str) ? str = str.replace(/</g, `<span>&lt;</span>`) : str = str
+    // //console.log(openBracketRegex.test(str))
+    // console.log(str)
+
+    const hasHeadings = str.match(headingsRegex)
+    hasHeadings ? str = headings(hasHeadings, str) : str = str
+
+    const hasLinks = str.match(linksRegex)
+    hasLinks ? str = links(hasLinks, str) : str = str
+
+    const hasBold = str.match(boldRegex)
+    hasBold ? str = bold(hasBold, str) : str = str
+
+    const hasItalic = str.match(italicRegex)
+    hasItalic ? str = italic(hasItalic, str) : str = str
+
+    const hasCode = str.match(codeRegex)
+    hasCode ? str = code(hasCode, str) : str = str
+
+    const hasImage = str.match(imageRegex)
+    hasImage ? str = image(hasImage, str) : str = str
+
+
+
+    str = str.replace(/\n{3,}/g, '</br></br>')
+    str = str.replace(/\n\n/g, '</br></br>')
+    str = str.replace(/(\n|\\\n)/g, '</br>')
+
+    return str;
 };
+
+// const bracket = (match, str) => {
+//     //const bracketOpen = /</g
+//     const badBracket = /(<.*>|<.*|<)/g
+//     const goodBracket = /(?<=`)"?<.*>.*<.*>"?(?=`)/g
+//     const codeMatch = /`?<code.*>.*<\/code>`?/g
+//     //const goodBracket = /(?<=`<.*>).*(?=<.*>`)/g
+
+//     match.forEach((m) => {
+//         let badMatch = m.match(badBracket)
+//         let goodMatch = m.match(goodBracket)
+//         console.log(m.match(codeMatch))
+//         if (m.match(codeMatch)) {
+//             return
+//         }
+
+//         if (goodMatch) {
+//             //console.log(goodMatch)
+//             str = str.replace(m, `<pre><code>${goodMatch[0]}</code></pre>`)
+//             return
+//         } else if (badMatch) {
+//             str = str.replace(m, '')
+//             return
+//         }
+//     })
+
+//     return str
+// }
+
+const openBrackets = (match, str) => {
+    const quoteTest = /`<.*>`/g
+    console.log(match)
+    match.forEach((m) => {
+        let qMatch = m.match(quoteTest)
+        console.log(qMatch)
+        if (!qMatch) {
+            str = str.replace(m, `&lt;`)
+            return
+        }
+    })
+
+    return str
+}
+
+const bracket = (match, str) => {
+    const codeMatch = /(?<=`)<.+>.*<\/.+>(?=`)/g
+
+    match.forEach((m) => {
+        let cMatch = m.match(codeMatch)
+        console.log(cMatch)
+        let hl = hljs.highlightAuto(`${cMatch[0]}`, ['html']).value
+        str = str.replace(m, `<blockquote><pre>${hl}</pre></blockquote>`)
+    })
+
+    return str
+}
+
+const headings = (match, str) => {
+    const h1 = /(?<=#\s).+\n?/g
+    const h2 = /(?<=##\s).+\n?/g
+    const h3 = /(?<=###\s).+\n?/g
+    
+    match.forEach((m) => {
+        let matched
+        
+        if (m.match(h3)) {
+            matched = m.match(h3)
+            str = str.replace(m, `<h3>${matched[0]}</h3>`)
+            return
+        } else if (m.match(h2)) {
+            matched = m.match(h2)
+            str = str.replace(m, `<h2>${matched[0]}</h2>`)
+            return
+        } else if (m.match(h1)) {
+            matched = m.match(h1)
+            str = str.replace(m, `<h1>${matched[0]}</h1>`)
+            return
+        }
+    })
+
+    return str
+}
+
+const links = (match, str) => {
+    const description = /(?<=\[).+(?=\])/g
+    const href = /(?<=\().+(?=\))/g
+
+    match.forEach((m) => {
+        let dMatch = m.match(description)
+        let hMatch = m.match(href)
+
+        str = str.replace(m, `<a href=${hMatch[0]}>${dMatch}</a>`)
+    })
+
+    return str
+}
+
+const bold = (match, str) => {
+    const boldText = /(?<=\*\*)[^*]+(?=\*\*)/g
+
+    match.forEach((m) => {
+        let bMatch = m.match(boldText)
+        
+        str = str.replace(m, `<b>${bMatch[0]}</b>`)
+    })
+
+    return str
+}
+
+const italic = (match, str) => {
+    const italicText = /(?<=\*)[^*]+(?=\*)/g
+
+    match.forEach((m) => {
+        let iMatch = m.match(italicText)
+
+        str = str.replace(m, `<i>${iMatch[0]}</i>`)
+    })
+
+    return str
+}
+
+const code = (match, str) => {
+    const codeRegex = /(?<=`)[^<>\n]+(?=`)/g
+
+    match.forEach((m) => {
+        let cMatch = m.match(codeRegex)
+
+        str = str.replace(m, `<code style="color:RoyalBlue;">${cMatch[0]}</code>`)
+    })
+
+    return str
+}
+
+const image = (match, str) => {
+    const description = /(?<=!\[).*(?=\])/g
+    const src = /(?<=\().+(?=\))/g
+
+    match.forEach((m) => {
+        let dMatch = m.match(description)
+        let sMatch = m.match(src)
+
+        str = str.replace(m, `<img src=${sMatch[0]} alt=${dMatch[0] || ''} />`)
+    })
+
+    return str
+}
+
+const blockCode = (match, str) => {
+    const innerCode = /(?<=```js\n)(.+\s)+\n?(?=```)/gm
+    
+    match.forEach((m) => {
+        let bcMatch = m.match(innerCode)
+        let hl = hljs.highlightAuto(`${bcMatch}`, ['html', 'javascript']).value
+        console.log(hl)
+        //str = str.replace(m, `<blockquote style="gray;width:400px;"><pre><code class="language-js" id='block'>${bcMatch}</code></pre></blockquote>`)
+        str = str.replace(m, `<blockquote><pre class="code-block">${hl}</pre></blockquote>`)
+    })
+
+    return str
+}
